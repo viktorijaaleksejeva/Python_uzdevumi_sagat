f01fca5110acf12c4805ee046c17147bb6a7c31f
Dtoce <54190061+Dtoce@users.noreply.github.com>
2022-05-17 17:04:43 +0300

    Labots

--- 5uzd.py
+++ 5uzd.py
@@ -2,3 +2,14 @@
 Kas jāraksta aiz "=" zīmes, lai iegūtu informāciju par objekta pilsētu?
 """
 
+
+class DATI:
+
+
+    def __init__(self, nosaukums, pilseta):
+        self.nosaukums = nosaukums
+        self.pilseta = pilseta
+        
+        
+        
+pirmais_ieraksts =DATI("klase203", "USA")
