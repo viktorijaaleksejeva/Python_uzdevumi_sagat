222dad37442c7430849946457610ffb4638aabd5
Dtoce <54190061+Dtoce@users.noreply.github.com>
2022-05-17 17:01:40 +0300

    Labots

--- 2uzd.py
+++ 2uzd.py
@@ -1,13 +1,5 @@
 """
 Iegūt ziņas, izmantojot rss no google.lv.
 
-
-RSS ( RDF Site Summary vai Really Simple Syndication )  ir tīmekļa plūsma,
- kas ļauj lietotājiem un lietojumprogrammām piekļūt vietņu atjauninājumiem 
- standartizētā , datorlasāmā formātā. Abonējot RSS plūsmas, lietotājs var 
- izsekot daudzām dažādām vietnēm vienā ziņu apkopotājā , kas pastāvīgi pārrauga
-  vietnes, lai atrastu jaunu saturu, tādējādi lietotājam vairs nav tās manuāli 
-  jāpārbauda. Ziņu apkopotājus (vai "RSS lasītājus") var iebūvēt pārlūkprogrammā,
-   instalēt galddatorā vai mobilajā ierīcē .
 """
 
