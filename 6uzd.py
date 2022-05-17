c5551a3be6e145a81901b55d4e01351dd46cef8f
Dtoce <54190061+Dtoce@users.noreply.github.com>
2022-05-17 17:06:26 +0300

    Labots

--- 6uzd.py
+++ 6uzd.py
@@ -5,14 +5,11 @@
 
 
 
-np2005=Nobel("Peace", 2005, "Muhammad Yunus")
-print(np2005.category, np2005.year, np2005.winner)
-
 
 
 
 kt2022=APRAKSTS("Vasara", 2015, "Kate Ziemele")
-print(kt2022.nosaukums, kt2022.gads, kt2022.autorsr)
+print(kt2022.nosaukums, kt2022.gads, kt2022.autors)
 
 
 
